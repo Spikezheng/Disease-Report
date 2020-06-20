c3213af9f70a6a9cf9a26c2cd561439b86be5739
BushiJhon <1172905107@qq.com>
2020-06-20 17:21:47 +0800

    医院用户类

---
+++ reportserver/src/main/java/com/bushi/diseasereportserver/pojo/Hospital.java
@@ -0,0 +1,59 @@
+package com.bushi.diseasereportserver.pojo;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+
+@Entity
+@Table(name = "hospital")
+public class Hospital {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "password")
+    private String password;
+
+    public Hospital(){}
+
+    public Hospital(String id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
+
+    public Hospital(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
