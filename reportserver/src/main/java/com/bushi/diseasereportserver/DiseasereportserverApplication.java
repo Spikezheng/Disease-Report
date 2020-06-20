cc44492e298398e47bae7f699a2440df995380c6
BushiJhon <1172905107@qq.com>
2020-06-20 17:22:40 +0800

    Springboot入口

---
+++ reportserver/src/main/java/com/bushi/diseasereportserver/DiseasereportserverApplication.java
@@ -0,0 +1,13 @@
+package com.bushi.diseasereportserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiseasereportserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiseasereportserverApplication.class, args);
+    }
+
+}
